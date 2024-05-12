df8dae4df6ce32bb4771eba67881d2357ce876d1
Sourabh Sain <114208070+DeveloprrSourabh@users.noreply.github.com>
2024-05-12 22:29:35 +0530

    Shop Page Filter Section completed

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import Layout from "../components/Layout";
 import PageBanner from "../components/PageBanner";
 import { Link, useLocation } from "react-router-dom";
@@ -6,6 +6,8 @@ import Productcard from "../components/Productcard";
 const Shop = () => {
   const location = useLocation();
   const page = location.pathname.replace("/", "");
+  const ref = useRef();
+  const filterRef = useRef();
   return (
     <>
       <Layout>
@@ -16,10 +18,362 @@ const Shop = () => {
           <div className="shop-page-start  mx-5 py-3 my-5">
             <div className="filter-row  d-flex hover justify-content-between">
               <div id="filter1" className="first-filter ">
-                <Link className="d-flex gap-1 footer-contact-desc">
+                <div
+                  onClick={() => {
+                    filterRef.current.classList.add("active");
+                    document
+                      .getElementById("main-body")
+                      .classList.add("active");
+                  }}
+                  className="d-flex gap-1 footer-contact-desc"
+                >
                   <img src="./images/filter.svg" width={"16px"} alt="" />
                   Filter
-                </Link>
+                </div>
+                <div ref={filterRef} className="filter-drawer hover">
+                  <div className="main-filter-heading d-flex justify-content-between ">
+                    <h2>FILTER</h2>
+                    <span
+                      onClick={() => {
+                        filterRef.current.classList.remove("active");
+                        document
+                          .getElementById("main-body")
+                          .classList.remove("active");
+                      }}
+                    >
+                      ✕
+                    </span>
+                  </div>
+                  <div className="main-filters">
+                    <div className="avaiable">
+                      <h2>Availability</h2>
+                      <div className="available-filter">
+                        <ul className="p-0 m-0">
+                          <li className="py-2">
+                            <input
+                              type="checkbox"
+                              id="vehicle1"
+                              name="vehicle1"
+                              value="Bike"
+                            />
+                            <label className="px-2" for="vehicle1">
+                              In Stock{"  "}
+                              <span className="filter-count"> (230)</span>
+                            </label>
+                            <br />
+                          </li>
+                          <li className="py-2">
+                            <input
+                              type="checkbox"
+                              id="vehicle1"
+                              name="vehicle1"
+                              value="Bike"
+                            />
+                            <label className="px-2" for="vehicle1">
+                              Out Of Stock{"  "}
+                              <span className="filter-count">(117)</span>
+                            </label>
+                            <br />
+                          </li>
+                          <li className="py-2">
+                            <input
+                              type="checkbox"
+                              id="vehicle1"
+                              name="vehicle1"
+                              value="Bike"
+                            />
+                            <label className="px-2" for="vehicle1">
+                              I have a bike{"  "}
+                              <span className="filter-count">(230)</span>
+                            </label>
+                            <br />
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div className="avaiable">
+                      <h2>Product type</h2>
+                      <div className="available-filter">
+                        <ul className="p-0 m-0">
+                          <li className="py-2">
+                            <input
+                              type="checkbox"
+                              id="vehicle1"
+                              name="vehicle1"
+                              value="Bike"
+                            />
+                            <label className="px-2" for="vehicle1">
+                              Acessories {"  "}
+                              <span className="filter-count"> (5)</span>
+                            </label>
+                            <br />
+                          </li>
+                          <li className="py-2">
+                            <input
+                              type="checkbox"
+                              id="vehicle1"
+                              name="vehicle1"
+                              value="Bike"
+                            />
+                            <label className="px-2" for="vehicle1">
+                              Bag{"  "}
+                              <span className="filter-count">(2)</span>
+                            </label>
+                            <br />
+                          </li>
+                          <li className="py-2">
+                            <input
+                              type="checkbox"
+                              id="vehicle1"
+                              name="vehicle1"
+                              value="Bike"
+                            />
+                            <label className="px-2" for="vehicle1">
+                              Camera{"  "}
+                              <span className="filter-count">(2)</span>
+                            </label>
+                            <br />
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                    <div className="avaiable">
+                      <h2>Color</h2>
+                      <div className="available-filter">
+                        <ul className="p-0 m-0">
+                          <li className="py-2">
+                            <span
+                              style={{ background: "Black" }}
+                              className="color-filter "
+                            >
+                              <input
+                                type="checkbox"
+                                id="vehicle1"
+                                name="vehicle1"
+                                value="Bike"
+                                className="opacity-0"
+                              />
+                            </span>
+                            <label className="px-2" for="vehicle1">
+                              Black{"  "}
+                              <span className="filter-count"> (230)</span>
+                            </label>
+                            <br />
+                          </li>
+                          <li className="py-2">
+                            <span
+                              style={{ background: "Red" }}
+                              className="color-filter "
+                            >
+                              <input
+                                type="checkbox"
+                                id="vehicle1"
+                                name="vehicle1"
+                                value="Bike"
+                                className="opacity-0"
+                              />
+                            </span>
+                            <label className="px-2" for="vehicle1">
+                              Red{"  "}
+                              <span className="filter-count">(10)</span>
+                            </label>
+                            <br />
+                          </li>
+                          <li className="py-2">
+                            <span
+                              style={{ background: "Red" }}
+                              className="color-filter "
+                            >
+                              <input
+                                type="checkbox"
+                                id="vehicle1"
+                                name="vehicle1"
+                                value="Bike"
+                                className="opacity-0"
+                              />
+                            </span>
+                            <label className="px-2" for="vehicle1">
+                              Red{"  "}
+                              <span className="filter-count">(10)</span>
+                            </label>
+                            <br />
+                          </li>
+                          <li className="py-2">
+                            <span
+                              style={{ background: "Red" }}
+                              className="color-filter "
+                            >
+                              <input
+                                type="checkbox"
+                                id="vehicle1"
+                                name="vehicle1"
+                                value="Bike"
+                                className="opacity-0"
+                              />
+                            </span>
+                            <label className="px-2" for="vehicle1">
+                              Red{"  "}
+                              <span className="filter-count">(10)</span>
+                            </label>
+                            <br />
+                          </li>
+                          <li className="py-2">
+                            <span
+                              style={{ background: "Red" }}
+                              className="color-filter "
+                            >
+                              <input
+                                type="checkbox"
+                                id="vehicle1"
+                                name="vehicle1"
+                                value="Bike"
+                                className="opacity-0"
+                              />
+                            </span>
+                            <label className="px-2" for="vehicle1">
+                              Red{"  "}
+                              <span className="filter-count">(10)</span>
+                            </label>
+                            <br />
+                          </li>
+                          <li className="py-2">
+                            <span
+                              style={{ background: "Red" }}
+                              className="color-filter "
+                            >
+                              <input
+                                type="checkbox"
+                                id="vehicle1"
+                                name="vehicle1"
+                                value="Bike"
+                                className="opacity-0"
+                              />
+                            </span>
+                            <label className="px-2" for="vehicle1">
+                              Red{"  "}
+                              <span className="filter-count">(10)</span>
+                            </label>
+                            <br />
+                          </li>
+                          <li className="py-2">
+                            <span
+                              style={{ background: "Red" }}
+                              className="color-filter "
+                            >
+                              <input
+                                type="checkbox"
+                                id="vehicle1"
+                                name="vehicle1"
+                                value="Bike"
+                                className="opacity-0"
+                              />
+                            </span>
+                            <label className="px-2" for="vehicle1">
+                              Red{"  "}
+                              <span className="filter-count">(10)</span>
+                            </label>
+                            <br />
+                          </li>
+                          <li className="py-2">
+                            <span
+                              style={{ background: "Red" }}
+                              className="color-filter "
+                            >
+                              <input
+                                type="checkbox"
+                                id="vehicle1"
+                                name="vehicle1"
+                                value="Bike"
+                                className="opacity-0"
+                              />
+                            </span>
+                            <label className="px-2" for="vehicle1">
+                              Red{"  "}
+                              <span className="filter-count">(10)</span>
+                            </label>
+                            <br />
+                          </li>
+                          <li className="py-2">
+                            <span
+                              style={{ background: "Red" }}
+                              className="color-filter "
+                            >
+                              <input
+                                type="checkbox"
+                                id="vehicle1"
+                                name="vehicle1"
+                                value="Bike"
+                                className="opacity-0"
+                              />
+                            </span>
+                            <label className="px-2" for="vehicle1">
+                              Red{"  "}
+                              <span className="filter-count">(10)</span>
+                            </label>
+                            <br />
+                          </li>
+                          <li className="py-2">
+                            <span
+                              style={{ background: "Red" }}
+                              className="color-filter "
+                            >
+                              <input
+                                type="checkbox"
+                                id="vehicle1"
+                                name="vehicle1"
+                                value="Bike"
+                                className="opacity-0"
+                              />
+                            </span>
+                            <label className="px-2" for="vehicle1">
+                              Red{"  "}
+                              <span className="filter-count">(10)</span>
+                            </label>
+                            <br />
+                          </li>
+                          <li className="py-2">
+                            <span
+                              style={{ background: "yellow" }}
+                              className="color-filter "
+                            >
+                              <input
+                                type="checkbox"
+                                id="vehicle1"
+                                name="vehicle1"
+                                value="Bike"
+                                className="opacity-0"
+                              />
+                            </span>
+                            <label className="px-2" for="vehicle1">
+                              Yellow{"  "}
+                              <span className="filter-count">(230)</span>
+                            </label>
+                            <br />
+                          </li>
+                          <li className="py-2">
+                            <span
+                              style={{ background: "green" }}
+                              className="color-filter "
+                            >
+                              <input
+                                type="checkbox"
+                                id="vehicle1"
+                                name="vehicle1"
+                                value="Bike"
+                                className="opacity-0"
+                              />
+                            </span>
+                            <label className="px-2" for="vehicle1">
+                              Green{"  "}
+                              <span className="filter-count">(230)</span>
+                            </label>
+                            <br />
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
               <div className="second-filter">
                 <div class="dropdown">
